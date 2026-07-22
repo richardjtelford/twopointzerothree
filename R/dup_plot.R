3768f86d9fc0ab5a6a2fbd62b3ee8b36f984fa74
RichardJTelford <Richard.Telford@uib.no>
2026-07-22 16:01:56 +0200

    simplify plot code

--- R/dup_plot.R
+++ R/dup_plot.R
@@ -19,20 +19,16 @@
 # multicolumn
 dup_plot_raster <- function(obj) {
   if (!any(colnames(obj) == "col1")) {
-    p1 <- ggplot(obj, aes(.data$pos1, .data$pos2, fill = .data$delta)) +
+    p1 <- ggplot(obj, aes(.data$pos1, .data$pos2, fill = .data$type)) +
       geom_raster()
-    if (any(colnames(obj) == "type")) {
-      p1 <- p1 + aes(fill = .data$type)
-    }
   } else {
     p1 <- ggplot(obj, aes(.data$row1, .data$row2, fill = .data$delta)) +
       geom_raster() +
       facet_grid(rows = vars(.data$col1), cols = vars(.data$col2))
-      
+
     if (any(colnames(obj) == "type")) {
-      p1 <- p1 + aes(fill =  .data$type)
+      p1 <- p1 + aes(fill = .data$type)
     }
-    
   }
   p1
 }
@@ -47,22 +43,20 @@ dup_plot_raster <- function(obj) {
 #' @importFrom tidyr pivot_longer
 #' @export
 dup_plot_lines <- function(obj) {
-  type <- if(any(colnames(obj) == "type")) "type" else NULL 
   if (!any(colnames(obj) == "col1")) {
-    obj |> 
-      select("duplicate_no", type, "pos1", "pos2") |> 
-      pivot_longer(c("pos1", "pos2"), names_to = "which", values_to = "pos") |> 
-      summarise(min = min(.data$pos), max = max(.data$pos), .by = c("duplicate_no", type, "which")) |> 
+    obj |>
+      select("duplicate_no", "type", "pos1", "pos2") |>
+      pivot_longer(c("pos1", "pos2"), names_to = "which", values_to = "pos") |>
+      summarise(min = min(.data$pos), max = max(.data$pos), .by = c("duplicate_no", "type", "which")) |>
       ggplot(aes(xmin = .data$min, xmax = .data$max, y = .data$duplicate_no)) +
-      geom_linerange(aes(colour = "type"))
+      geom_linerange(aes(colour = .data$type))
   } else {
-    obj |> 
-      select("duplicate_no", type, "row1", "row2", "col1", "col2") |> 
-      pivot_longer(c("row1", "row2"), names_to = "which", values_to = "pos") |> 
-      summarise(min = min(.data$pos), max = max(.data$pos), .by = c("duplicate_no", type, "col1", "col2", "which")) |> 
+    obj |>
+      select("duplicate_no", "type", "row1", "row2", "col1", "col2") |>
+      pivot_longer(c("row1", "row2"), names_to = "which", values_to = "pos") |>
+      summarise(min = min(.data$pos), max = max(.data$pos), .by = c("duplicate_no", "type", "col1", "col2", "which")) |>
       ggplot(aes(xmin = .data$min, xmax = .data$max, y = .data$duplicate_no)) +
-      geom_linerange(aes(colour = "type")) +
+      geom_linerange(aes(colour = .data$type)) +
       facet_grid(rows = vars(.data$col1), cols = vars(.data$col2))
   }
-  
 }
